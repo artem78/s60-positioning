904978efdaf72b59363a07e9a944de27085f458d
artem78 <megabyte1024@ya.ru>
2023-07-02 10:53:18 +0300

    Version 1.0.2

--- Positioning.h
+++ Positioning.h
@@ -2,7 +2,7 @@
  ============================================================================
  Name		: Positioning.h
  Author	  : artem78
- Version	 : 1.0.1
+ Version	 : 1.0.2
  Copyright   : 
  Description : Receiving location info
  ============================================================================
